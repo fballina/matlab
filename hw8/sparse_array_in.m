f4f09ebccc1bbd4b77b33ab4ed1559fb855ab1cd
junzhiye <junzhiye57@gmail.com>
2015-05-25 12:32:49 -0400

    Problem 2

---
+++ hw8/sparse_array_in.m
@@ -0,0 +1,27 @@
+function [out] = sparse_array_in(infile)
+    fileID = fopen(infile);
+    if fileID == -1
+        out = [];
+        return
+    end
+    
+    r = fread(fileID, 1, 'uint32');
+    c = fread(fileID, 1, 'uint32');
+    if r == 0 || c == 0
+        out = zeros(r, c); 
+        return;
+    end
+    
+    out = zeros(r, c); 
+    fread(fileID, 1, 'uint32');
+    
+    while ~feof(fileID)
+        i = fread(fileID, 1, 'uint32');
+        j = fread(fileID, 1, 'uint32');
+        x = fread(fileID, 1, 'double');
+        out(i,j) = x;
+    end
+    fclose(fileID); 
+end
+    
+    
